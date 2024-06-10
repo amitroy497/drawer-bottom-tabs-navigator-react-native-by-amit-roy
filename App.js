8c52f97c1773b401909a69fc7c3d77062c96cc63
Amit Roy <amitroy497@yahoo.com>
2024-06-10 20:11:18 +0530

    feat(bottom-tabs-navigation): bottom tabs navigation in detail

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { DrawerNavigation } from './src/navigation';
+import { BottomTabsNavigation } from './src/navigation/BottomTabsNavigation';
 
 export default function App() {
 	return (
 		<>
 			<DrawerNavigation />
+			<BottomTabsNavigation />
 		</>
 	);
 }
